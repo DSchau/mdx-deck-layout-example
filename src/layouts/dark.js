fd886bba8a762785024e749e50e1fe49bd16d030
Dustin Schau <dustinschau@gmail.com>
2018-09-24 12:34:18 -0500

    feat: initial commit

---
+++ src/layouts/dark.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Dark = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+
+  align-items: center;
+  justify-content: center;
+
+  background-color: black;
+  color: white;
+`
+
+export default Dark
